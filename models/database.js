e0fd9eba9788718608785891909de56c11e08edf
Aaron Finkenzeller <aaronfinkenzeller@gmail.com>
2015-08-14 19:14:49 -0400

    updated info

--- models/database.js
+++ models/database.js
@@ -1,5 +1,5 @@
 var pg = require('pg')
-var connectionString = process.env.DATABASE_URL ||'postgres://aaron:newone@localhost:5432/todo';
+var connectionString = process.env.DATABASE_URL ||'postgres://localhost:5432/todo';
 
 var client = new pg.Client(connectionString)
 client.connect();
